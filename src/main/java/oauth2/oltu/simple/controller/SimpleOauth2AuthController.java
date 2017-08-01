f4a8cf462eef77cd4b46288ef2fbc64188ce601a
yuezh2 <yuezh2@YUEZH2-PC07B6G2.lenovo.com>
2017-08-01 17:52:44 +0800

    oltu获取accesstoken方法提交

--- src/main/java/oauth2/oltu/simple/controller/SimpleOauth2AuthController.java
+++ src/main/java/oauth2/oltu/simple/controller/SimpleOauth2AuthController.java
@@ -1,11 +1,14 @@
 package oauth2.oltu.simple.controller;
 
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.oltu.oauth2.as.issuer.OAuthIssuer;
 import org.apache.oltu.oauth2.as.request.OAuthAuthzRequest;
+import org.apache.oltu.oauth2.as.request.OAuthTokenRequest;
 import org.apache.oltu.oauth2.as.response.OAuthASResponse;
 import org.apache.oltu.oauth2.common.exception.OAuthProblemException;
 import org.apache.oltu.oauth2.common.message.OAuthResponse;
@@ -34,7 +37,81 @@ public class SimpleOauth2AuthController {
 	private SimpleOauth2AuthService service;
 	
 	
+	
+	
 	/**
+	 * 
+	 * 请求url:
+	 * 		http://localhost:8081/yzh-practice/simple/oauth/token?client_id=test&client_secret=test&grant_type=authorization_code&code=ac0bd18863b07adfb518cc6e6dfcfcab&redirect_uri=http://localhost:8081/yzh-practice/oauth/authorize?response_type=code&scope=read%20write&client_id=test&redirect_uri=http%3A%2F%2Flocalhost%3A7777%2Fspring-oauth-client%2Fauthorization_code_callback&state=09876999
+	 * 
+	 * 返回token
+	 * @return
+	 */
+	@RequestMapping(value="token")
+	public void token(Model model , HttpServletRequest request , HttpServletResponse response) {
+		OAuthTokenRequest oauthRequest = null;
+		 
+		    try {
+		           oauthRequest = new OAuthTokenRequest(request);
+		        
+//		           validateClient(oauthRequest);
+		 
+		           String authzCode = oauthRequest.getCode();
+		           
+		           //校验code
+		           
+		           
+		 
+		           // some code
+		           String accessToken = oAuthIssuer.accessToken();
+		           String refreshToken = oAuthIssuer.refreshToken();
+		 
+		           // some code
+		            OAuthResponse r = OAuthASResponse
+		                .tokenResponse(HttpServletResponse.SC_OK)
+		                .setAccessToken(accessToken)
+		                .setExpiresIn("3600")
+		                .setRefreshToken(refreshToken)
+		                .buildJSONMessage();
+		 
+		        response.setStatus(r.getResponseStatus());
+		        PrintWriter pw = response.getWriter();
+		        pw.print(r.getBody());
+		        pw.flush();
+		        pw.close();
+		         //if something goes wrong
+		    } catch(Exception ex) {
+		    	
+		    	if(ex instanceof OAuthProblemException){
+		    		try{
+				         final OAuthResponse resp = OAuthASResponse
+				             .errorResponse(HttpServletResponse.SC_FOUND)
+				             .error((OAuthProblemException)ex)
+				             .location(Constants.ERROR_URL)
+				             .buildQueryMessage();
+				 
+				         response.sendRedirect(resp.getLocationUri());
+		    		}catch(Exception e){
+		    			//nothing
+		    			e.printStackTrace();
+		    		}
+		    	}
+		    	
+		    	ex.printStackTrace();
+		    }
+	}
+	
+	
+	
+	
+	
+	
+	
+	/**
+	 * 
+	 * 请求url:
+	 * 		localhost:8081/yzh-practice/simple/oauth/authorize?response_type=code&scope=read write&client_id=test&state=09876999
+	 * 
 	 * 返回code
 	 * @return
 	 */
