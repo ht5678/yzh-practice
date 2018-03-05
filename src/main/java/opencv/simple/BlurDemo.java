847b3c96508e765d141e215b28fdd911ebf4856a
sdwhy <sdwhy@yue>
2018-03-06 01:09:26 +0800

    美容算法提交

--- src/main/java/opencv/simple/BlurDemo.java
+++ src/main/java/opencv/simple/BlurDemo.java
@@ -1,6 +1,7 @@
 package opencv.simple;
 
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Size;
@@ -18,7 +19,10 @@ import org.opencv.imgproc.Imgproc;
  * 双边滤波
  * 
  * 
- * ksize卷积和的大小要是奇数 , 1,3,5 , 偶数不好处理
+ * ksize卷积和的大小必须是奇数 , 1,3,5 , 偶数不好找中心点 ，表示矩阵的大小
+ * 
+ * 应用场景:
+ * 去除椒盐噪声 , 用来美容 , 去除白点黑点
  * 
  * @author sdwhy
  *
@@ -42,7 +46,32 @@ public class BlurDemo {
 	    //高斯模糊
 	    Mat gDst = new Mat(src.size() , src.type());
 	    Imgproc.GaussianBlur(src, gDst, new Size(11,11), 11,11);
-	    Imgcodecs.imwrite("d://pics/gblur.jpg", dst);
+	    Imgcodecs.imwrite("d://pics/gblur.jpg", gDst);
+	    
+	    //中值模糊
+	    Mat mDst = new Mat(src.size() , src.type());
+	    Imgproc.medianBlur(src, mDst, 3);
+	    Imgcodecs.imwrite("d://pics/mblur.jpg", mDst);
+	    
+	    //双边模糊
+	    //15 - d , 计算的半径,半径之内的像素都会被纳入计算 , 如果提供-1 , 则根据sigma space参数取值
+	    //150 - sigma color  决定多少差值之内的像素会被计算
+	    //3 -  sigma space  , 如果d的值大于0则声明无效 , 否则根据他计算d的值
+	    Mat bDst = new Mat(src.size() , src.type());
+	    Imgproc.bilateralFilter(src, bDst, 15, 150, 3);
+	    Imgcodecs.imwrite("d://pics/bblur.jpg", bDst);
+	    
+	    //提升对比度
+	    int data[] = {  0, -1, 0, -1, 5, -1, 0, -1, 0 };
+	    Mat kernel = new Mat( 3, 3, CvType.CV_32S );
+		kernel.put( 0, 0, data );
+		
+	    Mat filterDst = new Mat().zeros(src.size(), src.type());
+		Imgproc.filter2D(bDst, filterDst, -1, kernel , new Point(-1,-1) , 0);		//src.depth()表示位图深度 , 有32 , 24 ,8 等  . 默认-1 , 表示和原图深度一样 , 不一样可能会出错
+		Imgcodecs.imwrite("d://pics/filterBlur.jpg", filterDst);
+	    
+	    
+	    
 	}
 	
 
