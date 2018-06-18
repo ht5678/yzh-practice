75156a6112eae19775da764f3f6cb2f0c44d55db
yue <sdwhyzhh@163.com>
2018-06-18 17:42:36 +0800

    反向投影demo初步提交

---
+++ src/main/java/opencv/simple/L27BackProjectDemo.java
@@ -0,0 +1,72 @@
+package opencv.simple;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfInt;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+/**
+ * 
+ *  #反向投影:
+	#
+	#*反向投影是反应直方图模型在目标图像中的分布情况
+	#*简单点说就是用直方图模型去目标图像中寻找是否有相似的对象 . 通常用HSV色彩空间的HS两个通道直方图模型
+	#
+	#步骤:
+	#*建立直方图模型
+	#*计算机待测图像直方图并映射到模型中
+	#*从模型反向计算生成图像
+	#
+	#
+	#
+	#相关API:
+	#*加载图片imread
+	#*将图像从RGB色彩空间转换到HSV色彩空间  cvtColor
+	#*计算直方图和归一化 calcHist与normalize
+	#*Mat和MatND其中Mat表示二维数组 , MatND表示三维或者多维数据 , 此处均可以用Mat表示
+	#*计算反向投影图像  calcBackProject
+ * 
+ * 
+ * 
+ * @author yuezh2   2018年6月18日 下午4:34:48
+ *
+ */
+public class L27BackProjectDemo {
+
+	
+	public static void main(String[] args) {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	    Mat src = Imgcodecs.imread("d://pics/222222.jpg");
+	    
+	    if(src.empty()){
+	    	System.out.println("图片地址不存在");
+	    	return;
+	    }
+	    Mat hsv = new Mat(src.size(),src.type());
+	    Imgproc.cvtColor(src, hsv, Imgproc.COLOR_BGR2HSV);
+	    
+	    List<Mat> srcs = new ArrayList<>();
+	    srcs.add(src);
+	    List<Mat> hsvs = new ArrayList<>();
+	    hsvs.add(hsv);
+	    
+	    Core.mixChannels(srcs, hsvs, new MatOfInt(0,0));
+	   
+	    
+	    Mat histBase = new Mat(hsv.size(),hsv.type());
+	    Imgproc.calcHist(hsvs, new MatOfInt(0), new Mat(), histBase, new MatOfInt(12), new MatOfFloat(0,180));
+	    Mat hue = new Mat(src.size(),src.type());
+	    Core.normalize(histBase, hue, 0, 256, Core.NORM_MINMAX,-1);
+	    
+	    Imgproc.calcBackProject(hsvs, new MatOfInt(0), hue, histBase, new MatOfFloat(0,180) , 1);
+	    
+	    Imgcodecs.imwrite("d://pics/backProject.jpg", histBase);
+	}
+	
+	
+}
