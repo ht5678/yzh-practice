4785bd2fb03b4352a812cd532ec5b93b0e1d2e96
sdwhy <sdwhy@yue>
2018-06-20 02:00:10 +0800

    发现轮廓提交

--- src/main/java/opencv/simple/L29FindContourDemo.java
+++ src/main/java/opencv/simple/L29FindContourDemo.java
@@ -1,6 +1,17 @@
 package opencv.simple;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
 /**
  * 轮廓发现:
@@ -47,5 +58,36 @@ package opencv.simple;
  *
  */
 public class L29FindContourDemo {
+	
+	
+	public static void main(String[] args) {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	    Mat src = Imgcodecs.imread("d://pics/wx.jpg");
+	    
+	    if(src.empty()){
+	    	System.out.println("图片地址不存在");
+	    	return;
+	    }
+	    
+	    Mat dst = new Mat(src.size(),src.type());
+	    Mat cannyOut = new Mat(src.size(),src.type());
+	    Imgproc.cvtColor(src, dst, Imgproc.COLOR_BGR2GRAY);
+	    
+	    int thresholdValue=1;
+	    List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+	    Mat hierarchy =  new Mat();
+	    		
+	    Imgproc.Canny(src, cannyOut, thresholdValue, thresholdValue*2,3,false);
+	    Imgproc.findContours(cannyOut, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE, new Point(0,0));
+	    
+	    dst = Mat.zeros(src.size(), CvType.CV_8UC3);
+	    Random rd = new Random(255);
+	    for(int i =0;i<contours.size();i++){
+	    	Scalar color = new Scalar(rd.nextInt(),rd.nextInt(),rd.nextInt());
+	    	Imgproc.drawContours(dst, contours, i, color,2,Core.LINE_8,hierarchy,0,new Point(0,0));
+	    }
+	    
+	    Imgcodecs.imwrite("d://pics/findContours.jpg", dst);
+	}
 
 }
