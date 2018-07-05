4875c42addac844c8cedae516496236f9dcc8002
yue <sdwhyzhh@163.com>
2018-07-05 12:50:53 +0800

    demo提交

--- src/main/java/opencv/simple/L31RDPDemo.java
+++ src/main/java/opencv/simple/L31RDPDemo.java
@@ -4,6 +4,7 @@ import java.awt.EventQueue;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -17,6 +18,9 @@ import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.RotatedRect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
@@ -76,7 +80,7 @@ public class L31RDPDemo {
 	 * 初始化
 	 */
 	private void initialize(){
-		Mat source = Imgcodecs.imread("d://pics//samples//palm_b.jpg");
+		Mat source = Imgcodecs.imread("d://pics//samples//coin.jpg");
 		BufferedImage image = matToBufferedImage(source);
 		frmjavaSwing = new JFrame();
 		frmjavaSwing.setTitle("轮廓周围绘制矩形框和圆形框");
@@ -118,14 +122,19 @@ public class L31RDPDemo {
 	
 	
 	public Mat findAndDrawPolygon(double threshold1){
-		Mat source = Imgcodecs.imread("d://pics//samples//palm_b.jpg",Imgcodecs.CV_LOAD_IMAGE_GRAYSCALE);
+		Mat source = Imgcodecs.imread("d://pics//samples//coin.jpg",Imgcodecs.CV_LOAD_IMAGE_GRAYSCALE);
 		Mat destination = new Mat(source.rows(),source.cols(),source.type());
 		Imgproc.GaussianBlur(source, source, new Size(3,3), 10,0);
 		Imgproc.threshold(source, destination, threshold1, 255, 0);
 		List<MatOfPoint> contours = new ArrayList<>();
 		Mat hierarchy = new Mat(source.rows(),source.cols(),CvType.CV_8UC1,new Scalar(0));
 		Imgproc.findContours(destination, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
-		
+
+		List<Rect> polyRects = new ArrayList<>();
+		List<Point> ccs = new ArrayList<>();
+		List<float[]> radius = new ArrayList<>();
+		List<RotatedRect> minRects = new ArrayList<>();
+		List<RotatedRect> myellipse = new ArrayList<>();
 		Mat drawing = Mat.zeros(destination.size(), CvType.CV_8UC3);
 		for(int i =0 ; i < contours.size();i++){
 			MatOfPoint2f mMOP2f1 = new MatOfPoint2f();
@@ -137,8 +146,31 @@ public class L31RDPDemo {
 			mMOP2f2.convertTo(contours.get(i), CvType.CV_32S);
 			Imgproc.drawContours(drawing, contours, i, new Scalar(255,0,0,255),2);
 			
-			Imgproc.minEnclosingCircle(mMOP2f2, center, radius);
+			Rect rect = Imgproc.boundingRect(contours.get(i));
+			polyRects.add(rect);
+			ccs.add(new Point());
+//			radius.add(0.0F);
+			float[] rs = new float[1];
+			radius.add(rs);
+			Imgproc.minEnclosingCircle(mMOP2f2, ccs.get(i), rs);
+			
+			if(mMOP2f2.dims()>5){
+				myellipse.add(Imgproc.fitEllipse(mMOP2f2));
+				minRects.add(Imgproc.minAreaRect(mMOP2f2));
+			}
 		}
+		
+		
+		//draw it
+		Random rd = new Random();
+//		source.copyTo(drawImg);
+		for(int i = 0 ; i<contours.size();i++){
+			Scalar color = new Scalar(rd.nextInt(255),rd.nextInt(255),rd.nextInt(255));
+			
+			Imgproc.rectangle(drawing, polyRects.get(i).tl() , polyRects.get(i).br(),  color);
+			Imgproc.circle(drawing, ccs.get(i), (int)radius.get(i)[0] , new Scalar(0,255,0),2);
+		}
+		
 		return drawing;
 	}
 	
