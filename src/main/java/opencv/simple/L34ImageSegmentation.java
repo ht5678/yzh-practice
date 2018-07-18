c43ffa9057532f65e43ef50b24143eddd0750c4d
sdwhy <sdwhy@yue>
2018-07-19 03:28:22 +0800

    功能提交

--- src/main/java/opencv/simple/L34ImageSegmentation.java
+++ src/main/java/opencv/simple/L34ImageSegmentation.java
@@ -1,11 +1,17 @@
 package opencv.simple;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
+import org.opencv.core.Scalar;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
@@ -79,7 +85,7 @@ public class L34ImageSegmentation {
 	
 	public static void main(String[] args) {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-	    Mat src = Imgcodecs.imread("d://pics/12345.png");
+	    Mat src = Imgcodecs.imread("d://pics/171717.jpg");
 	    
 	    if(src.empty()){
 	    	System.out.println("图片地址不存在");
@@ -98,6 +104,7 @@ public class L34ImageSegmentation {
 	    		}
 	    	}
 	    }
+	    System.out.println("black background");
 	    
 	    //sharpen锐化
 		Mat kernel =new Mat(3,3, CvType.CV_32F);
@@ -111,9 +118,76 @@ public class L34ImageSegmentation {
 		Imgproc.filter2D(src, imgLaplance, CvType.CV_32F, kernel, new Point(-1,-1) , 0, Core.BORDER_DEFAULT);
 		src.convertTo(sharpenImg, CvType.CV_32F);
 		Core.subtract(sharpenImg, imgLaplance, resultImg);
+		System.out.println("sharpen image");
 		
+		resultImg.convertTo(resultImg, CvType.CV_8UC3);
+		imgLaplance.convertTo(imgLaplance, CvType.CV_8UC3);
 		
-	    Imgcodecs.imwrite("d://pics/imageSegmentation.jpg", resultImg);
+		//convert to binary
+		Imgproc.cvtColor(src, resultImg, Imgproc.COLOR_BGR2GRAY);
+		Mat binaryImg = new Mat(resultImg.size(),resultImg.type());
+		Imgproc.threshold(resultImg, binaryImg, 40, 255, Imgproc.THRESH_BINARY|Imgproc.THRESH_OTSU);
+		System.out.println("binary image");
+		
+		Mat distImg = new Mat(src.size(),src.type());
+		Imgproc.distanceTransform(binaryImg, distImg, Imgproc.DIST_L1 , 3 , 5);
+		Core.normalize(distImg, distImg, 0, 1, Core.NORM_MINMAX);
+		System.out.println("distance result");
+		
+		//binary again
+		Imgproc.threshold(distImg, distImg, 0.4, 1, Imgproc.THRESH_BINARY);
+		Mat k1 = Mat.zeros(13, 13, CvType.CV_8UC1);
+//		Imgproc.erode(distImg, distImg, k1,new Point(-1,-1));
+		Imgproc.erode(distImg, distImg, k1);
+		System.out.println("distance binary image ");
+		
+		//markers
+		Mat dist8u = new Mat(src.size(),src.type());
+		distImg.convertTo(dist8u, CvType.CV_8U);
+		Mat hierarchy = new Mat(dist8u.rows(),dist8u.cols(),CvType.CV_8UC1,new Scalar(0));
+        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+       
+        //尋找外圍輪廓
+        Imgproc.findContours(dist8u, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+      
+        Mat markers = Mat.zeros(src.size(), CvType.CV_32SC1);
+        Random rd = new Random(255);
+        for(int i =0;i<contours.size();i++){
+	    	Scalar color = new Scalar(rd.nextInt(),rd.nextInt(),rd.nextInt());
+	    	Imgproc.drawContours(markers, contours, i, color,-1);
+	    }
+        Imgproc.circle(markers, new Point(5,5), 3, new Scalar(255,255,255),-1);
+        System.out.println("my markers");
+        
+        //perform watershed
+        Imgproc.watershed(src, markers);
+        Mat mark = Mat.zeros(markers.size(), CvType.CV_8UC1);
+        markers.convertTo(mark, CvType.CV_8UC1);
+        Core.bitwise_not(mark, mark,new Mat());
+        System.out.println("watershed image");
+        
+        //generate random color
+        Scalar[] colors = new Scalar[contours.size()];
+        for(int i = 0 ; i < contours.size();i++){
+        	Scalar color = new Scalar(rd.nextInt(),rd.nextInt(),rd.nextInt());
+        	colors[i]=color;
+        }
+		
+        //fill with color and display final result
+        Mat dst = Mat.zeros(markers.size(), CvType.CV_8UC3);
+        for (int row = 0; row < markers.rows(); row++) {
+    		for (int col = 0; col < markers.cols(); col++) {
+    			int index = (int)markers.get(row, col)[0];
+    			if (index > 0 && index <= contours.size()) {
+    				dst.put(row, col, colors[index - 1].val);
+    			}
+    			else {
+    				dst.put(row, col, new double[]{0,0,0});
+    			}
+    		}
+    	}
+        
+	    Imgcodecs.imwrite("d://pics/imageSegmentation.jpg", dst);
 		
 	}
 	
