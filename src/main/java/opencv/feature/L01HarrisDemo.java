166299a16595fa9bbddb86fa1d74bfd7db33b2f1
sdwhy <sdwhy@yue>
2018-07-20 02:51:58 +0800

    图像特征提取初始化提交

---
+++ src/main/java/opencv/feature/L01HarrisDemo.java
@@ -0,0 +1,37 @@
+package opencv.feature;
+
+
+/**
+ * 
+ * 图像的特征:
+ * 边缘
+ * 角点
+ * 纹理
+ * 
+ * 必须用灰度图像
+ * cornorHarris(
+ * InputArray src,
+ * OutputArray dst,
+ * int blockSize,
+ * int ksize,
+ * double k,
+ * int borderType=BORDER_DEFAULT
+ * )
+ * 
+ * 
+ * blocksize计算λ1λ2时候的矩阵大小
+ * ksize窗口大小
+ * k表示计算角度响应时候的参数大小
+ * 默认在0.04-0.06之间
+ * -阈值t,用来过滤角度响应
+ * 
+ * 
+ * @author sdwhy
+ *
+ */
+public class L01HarrisDemo {
+
+	
+	
+	
+}
