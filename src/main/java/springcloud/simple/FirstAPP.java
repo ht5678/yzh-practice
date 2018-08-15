7b04ed0840eaac5d5327d55c4cc8dbb005f796ec
yue <sdwhyzhh@163.com>
2018-08-15 19:25:01 +0800

    springcloud

---
+++ src/main/java/springcloud/simple/FirstAPP.java
@@ -0,0 +1,42 @@
+package springcloud.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * 
+ *         ┏┓　　　┏┓
+ *      ┏┛┻━━━┛┻┓
+ *      ┃　　　　　　　┃ 　
+ *      ┃　　　━　　　┃
+ *      ┃　┳┛　┗┳　┃
+ *      ┃　　　　　　　┃
+ *      ┃　　　┻　　　┃
+ *      ┃　　　　　　　┃
+ *      ┗━┓　　　┏━┛
+ *         ┃　　　┃　　　　　　　　　
+ *         ┃　　　┃
+ *         ┃　　　┗━━━┓
+ *         ┃　　　　　　　┣┓
+ *         ┃　　　　　　　┏┛
+ *         ┗┓┓┏━┳┓┏┛
+ *　　      ┃┫┫　┃┫┫
+ *　        ┗┻┛　┗┻┛
+ *
+ *-------------------- 神兽保佑 永无bug --------------------
+ * 
+ * 
+ * 
+ * @author yuezh2   2018年8月15日 上午11:35:59
+ *
+ */
+@SpringBootApplication
+public class FirstAPP {
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAPP.class, args);
+	}
+
+}
